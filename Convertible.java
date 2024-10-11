b7cc38194499fe69b53d77b5d8d3fadbb235c56f
azizahicha <icha_azizah_33rpl@student.smktelkom-mlg.sch.id>
2024-10-11 08:46:13 +0700

    Add files via upload

---
+++ Convertible.java
@@ -0,0 +1,25 @@
+public class Convertible extends car {
+    String roofType;
+
+    //Constructor Parameter
+
+    public String getRoofType() {
+        return this.roofType;
+    }
+
+    public void setRoofType(String roofType) {
+        this.roofType = roofType;
+    }
+
+    public Convertible(){
+        super();
+        roofType = "";
+
+        }
+
+    @Override
+    public void print (){
+        super.print();
+        System.out.println("Roof type of the car: "+roofType);
+    }
+}
